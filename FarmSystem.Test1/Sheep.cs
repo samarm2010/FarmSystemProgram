eb55af497e267e70caa04dd24c1d719984377484
samarm2010 <69417406+samarm2010@users.noreply.github.com>
2021-06-02 15:09:37 +1200

    Add files

---
+++ FarmSystem.Test1/Sheep.cs
@@ -0,0 +1,46 @@
+﻿using FarmSystem.Test2;
+using System;
+
+namespace FarmSystem.Test1
+{
+    public class Sheep : Animal
+    {
+       
+        private string _id;
+        private int _noOfLegs;
+
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+
+        public int NoOfLegs
+        {
+            get
+            {
+                return _noOfLegs;
+            }
+            set
+            {
+                _noOfLegs = 4;
+            }
+        }
+
+
+        public void Talk()
+        {
+            Console.WriteLine("Sheep says baa!");
+        }
+        
+        public void Run()
+        {
+            Console.WriteLine("Sheep is running");
+        }       
+    }
+
+}
